d8182a2eadb402a40ea91bf3c21356aecd66e74b
Maksim Shuklin <makshuk@bk.ru>
2024-04-12 13:50:43 +0300

    Добавить зависимость @googleapis/docs и googleapis в package.json

--- src/google-api/google-api.controller.ts
+++ src/google-api/google-api.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { GoogleApiService } from './google-api.service';
+import { Response, Request } from 'express';
 
 @Controller('google-api')
 export class GoogleApiController {
   constructor(private readonly googleApiService: GoogleApiService) {}
+
+  @Get('login')
+  async getAll(@Res() res: Response) {
+    const url = await this.googleApiService.start();
+    res.status(302).redirect(url);
+  }
+
+  @Get('redirect')
+  async getRedirect(@Res() res: Response, @Req() req: Request) {
+    console.log('Redirecting...');
+    await this.googleApiService.redirect(req);
+    return res.status(200).send('Redirected');
+  }
+
+  @Get('events')
+  async getEvents(@Res() res: Response) {
+    const events = await this.googleApiService.getEvents();
+    res.status(200).send(events);
+  }
 }
--- src/google-api/google-api.service.ts
+++ src/google-api/google-api.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { google } from 'googleapis';
+import { Request } from 'express';
+import { OAuth2Client } from 'google-auth-library';
 
 @Injectable()
-export class GoogleApiService {}
+export class GoogleApiService {
+  oauth2Client: OAuth2Client;
+
+  async start() {
+    this.oauth2Client = new google.auth.OAuth2(
+      process.env.YOUR_CLIENT_ID,
+      process.env.YOUR_CLIENT_SECRET,
+      process.env.YOUR_REDIRECT_URL,
+    );
+
+    const scopes = [
+      'https://www.googleapis.com/auth/calendar',
+      'https://www.googleapis.com/auth/tasks',
+    ];
+    const url = this.oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: scopes,
+    });
+
+    return url;
+  }
+
+  async redirect(req: Request) {
+    const code = req.query.code as string;
+
+    const { tokens } = await this.oauth2Client.getToken(code);
+    
+    this.oauth2Client.setCredentials(tokens);
+    console.log('Tokens:', tokens);
+  }
+
+  async getEvents() {
+    if (!this.oauth2Client.credentials) {
+      throw new Error('OAuth2Client has no credentials set');
+    }
+
+    const calendar = google.calendar({
+      version: 'v3',
+      auth: process.env.GOOGLE_API_KEY,
+    });
+
+    const tasks = google.tasks({
+      version: 'v1',
+      auth: process.env.GOOGLE_API_KEY,
+    });
+
+    const taskLists = await tasks.tasklists.list({ auth: this.oauth2Client });
+    console.log(taskLists);
+    return taskLists.data.items;
+  }
+}
